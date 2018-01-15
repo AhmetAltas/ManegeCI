b223350466eebf58e94894156d98d7f374b57371
DESKTOP-6PN09PQ\PC <99040436@mydavinci.nl>
2018-01-15 23:46:39 +0100

    Contact pagina begonnen

--- Manege/application/views/header.php
+++ Manege/application/views/header.php
@@ -22,29 +22,40 @@
 <body>
 
 	<header id="site-header">
-		<nav class="navbar navbar-default" role="navigation">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="<?= base_url() ?>">Site title</a>
-				</div>
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav navbar-right">
-						<?php if (isset($_SESSION['username']) && $_SESSION['logged_in'] === true) : ?>
-							<li><a href="<?= base_url('logout') ?>">Logout</a></li>
-						<?php else : ?>
-							<li><a href="<?= base_url('register') ?>">Register</a></li>
-							<li><a href="<?= base_url('login') ?>">Login</a></li>
-						<?php endif; ?>
-					</ul>
-				</div><!-- .navbar-collapse -->
-			</div><!-- .container-fluid -->
-		</nav><!-- .navbar -->
+		<style>
+			ul {
+			    list-style-type: none;
+			    margin: 0;
+			    padding: 0;
+			    overflow: hidden;
+			    background-color: #333;
+			}
+
+			li {
+			    float: left;
+			}
+
+			li a {
+			    display: block;
+			    color: white;
+			    text-align: center;
+			    padding: 14px 16px;
+			    text-decoration: none;
+			}
+
+			li a:hover {
+			    background-color: #111;
+			}
+</style>
+
+<ul>
+  <li><a class="active" href="/manege/index.php/paarden/view/">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="../../../../manege/index.php/contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+
+
 	</header><!-- #site-header -->
 
 	<main id="site-content" role="main">
---
+++ Manege/application/views/paarden/contact/contact.php
@@ -0,0 +1,10 @@
+<div>
+<h2>
+Contact Pagina	
+</h2>
+</div>
+
+<table>
+	
+
+</table>
