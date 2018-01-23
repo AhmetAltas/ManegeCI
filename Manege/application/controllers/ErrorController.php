76a272e2956de5e4d3ec281be41dfaa4510d314f
AhmetAltas <99040436@mydavinci.nl>
2018-01-24 00:00:05 +0100

    Error displayen

---
+++ Manege/application/controllers/ErrorController.php
@@ -0,0 +1,16 @@
+<?php
+//function index() 
+//{echo "Probleem! 404 ofzo.";
+//}
+function error404() 
+{
+	echo "Probleem! 404 ofzo.";
+}
+function databasefout()
+{
+	echo "Database actie kon niet uitgevoerd worden.";
+}
+function editfout()
+{
+	echo "Het is niet gelukt om de data aan te passen.";
+}
